60272a99907920a430c9220865e174f8d49d8002
Silvio Sergio Bestetti <sbestetti@MacBook-Pro-6.local>
2020-07-10 23:28:19 +0100

    Better button distribution with sticky

--- app/main.py
+++ app/main.py
@@ -1,21 +1,29 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.minsize(240, 320)
+
+for i in range(3):
+    window.rowconfigure(i, weight=1, minsize=80)
+
+for i in range(2):
+    window.columnconfigure(i, weight=1, minsize=120)
+
+
+# window.minsize(240, 320)
 
 btn_1 = tk.Button(master=window, text="Quarto")
-btn_1.grid(row=0, column=0, pady=3)
+btn_1.grid(row=0, column=0, pady=3, sticky="nsew")
 
 btn_2 = tk.Button(master=window, text="Banheiro")
-btn_2.grid(row=1, column=0, pady=3)
+btn_2.grid(row=1, column=0, pady=3, sticky="nsew")
 
 btn_3 = tk.Button(master=window, text="Corredor")
-btn_3.grid(row=2, column=0, pady=3)
+btn_3.grid(row=2, column=0, pady=3, sticky="nsew")
 
 btn_4 = tk.Button(master=window, text="<<")
-btn_4.grid(row=4, column=0, pady=3)
+btn_4.grid(row=4, column=0, pady=3, sticky="nsew")
 
 btn_5 = tk.Button(master=window, text=">>")
-btn_5.grid(row=4, column=1, pady=3)
+btn_5.grid(row=4, column=1, pady=3, sticky="nsew")
 
 window.mainloop()
