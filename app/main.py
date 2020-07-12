068eef339541d0aba91457a01453cd6f9a2fa855
Silvio Sergio Bestetti <sbestetti@MacBook-Pro-6.local>
2020-07-12 01:41:33 +0100

    Loop for creating buttons dynamically

--- app/main.py
+++ app/main.py
@@ -1,29 +1,36 @@
 import tkinter as tk
+import math
+
+import config
+
+number_of_buttons = len(config.rooms)
+number_of_pages = math.ceil(number_of_buttons/3)
 
 window = tk.Tk()
 
-for i in range(4):
+# Creates rows according to the number of rooms
+# on the config file
+for i in range(len(config.rooms)+1):
     window.rowconfigure(i, weight=1, minsize=75)
-
 for i in range(2):
     window.columnconfigure(i, weight=1, minsize=120)
 
-btn_1 = tk.Button(master=window, text="Quarto")
-btn_1.grid(row=0, column=0, pady=1, sticky="nsew", columnspan=2)
+# Adds one button for each room on the config file
+i = 0
+for key in config.rooms.keys():
+    tk.Button(master=window, text=key).grid(
+        row=i,
+        column=0,
+        pady=1,
+        sticky="nsew",
+        columnspan=2
+        )
+    i += 1
 
-btn_2 = tk.Button(master=window, text="Banheiro")
-btn_2.grid(row=1, column=0, pady=1, sticky="nsew", columnspan=2)
-
-btn_3 = tk.Button(master=window, text="Corredor")
-btn_3.grid(row=2, column=0, pady=1, sticky="nsew", columnspan=2)
-
-frm_bottom = tk.Frame(master=window)
-frm_bottom.grid(row=3, column=0, pady=1, sticky="nsew", columnspan=2)
-
-btn_4 = tk.Button(master=window, text="<<")
-btn_4.grid(row=3, column=0, pady=1, sticky="nsew")
-
-btn_5 = tk.Button(master=window, text=">>")
-btn_5.grid(row=3, column=1, pady=1, sticky="nsew")
+# Adds the buttons to navigate between pages
+btn_back = tk.Button(master=window, text="<<")
+btn_back.grid(row=len(config.rooms), column=0, pady=1, sticky="nsew")
+btn_fwd = tk.Button(master=window, text=">>")
+btn_fwd.grid(row=len(config.rooms), column=1, pady=1, sticky="nsew")
 
 window.mainloop()
